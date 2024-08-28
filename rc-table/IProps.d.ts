853507e44e03270811fd0ffcb4a477e301bbef18
Marvin <yongfeide123@gmail.com>
2024-08-28 23:47:20 +0800

    feat: table

--- rc-table/IProps.d.ts
+++ rc-table/IProps.d.ts
@@ -1,3 +1,127 @@
+enum ValueTypeEnum {
+  password = '密码输入框',
+  money = '金额输入框',
+  textarea = '文本域',
+  date = '日期',
+  dateTime = '日期时间',
+  dateWeek = '周',
+  dateMonth = '月',
+  dateQuarter = '季度输入',
+  dateYear = '年份输入',
+  dateRange = '日期区间',
+  dateTimeRange = '日期时间区间',
+  time = '时间',
+  timeRange = '时间区间',
+  text = '文本框',
+  select = '下拉框',
+  treeSelect = '树形下拉框',
+  checkbox = '多选框',
+  rate = '星级组件',
+  radio = '单选框',
+  radioButton = '按钮单选框',
+  progress = '进度条',
+  percent = '百分比组件',
+  digit = '数字输入框',
+  second = '秒格式化',
+  avatar = '头像',
+  code = '代码框',
+  switch = '开关',
+  fromNow = '相对于当前时间',
+  image = '图片',
+  jsonCode = '代码框，但是带了 json 格式化',
+  color = '颜色选择器',
+  cascader = '级联选择器',
+  segmented = '分段器',
+  group = '分组',
+  formList = '表单列表',
+  formSet = '表单集合',
+  divider = '分割线',
+  dependency = '依赖项'
+}
+
+interface Column {
+  /**
+   * 标题
+   *
+   * @title 标题
+   * @placeholder 标题
+   * @type string
+   */
+  title: string;
+
+  /**
+   * dataIndex
+   *
+   * @title dataIndex
+   * @placeholder 如 id
+   * @type string
+   */
+  dataIndex: string;
+
+  /**
+   * 值类型
+   *
+   * @title 值类型
+   * @type string
+   * @placeholder 请选择渲染类型
+   * @defalut text
+   */
+  valueType: ValueTypeEnum;
+
+
+  /**
+   * 过长省略
+   *
+   * @title 过长省略
+   * @type boolean
+   */
+  ellipsis: boolean;
+
+  /**
+   * 是否是搜索项
+   *
+   * @title 是否是搜索项
+   * @type boolean
+   * @default false
+   */
+  search: boolean;
+
+  /**
+   * 查询表单中不展示此项
+   *
+   * @title 查询表单中不展示此项
+   * @type boolean
+   * @default true
+   */
+  hideInSearch: boolean;
+
+  /**
+   * 复制按钮
+   *
+   * @title 复制按钮
+   * @type boolean
+   */
+  copyable: boolean;
+
+  /**
+   * 列提示
+   *
+   * @title 列提示
+   * @placeholder 请输入列提示
+   * @type string
+   */
+  tooltip: string;
+
+  /**
+   * 自定义渲染
+   *
+   * @title 自定义渲染
+   * @placeholder 请输入自定义渲染逻辑，建议绑定函数
+   * @type string
+   */
+  render: string;
+}
+
 /**
  * rc-table
  * @Component RcTable
@@ -5,21 +129,29 @@
  */
 export default interface RcTableIProps {
   /**
-   * 名称
+   * 数据列
    *
-   * @title 名称
-   * @type string
-   * @default 测试
+   * @title 数据列
+   * @minItems 1
+   * @maxItems 3
+   * @type Array
    */
-  name: string;
+  columns: Column[];
 
   /**
-   * 年龄
+   * rowKey
    *
-   * @title 年龄
-   * @min 0
-   * @max 100
+   * @title rowKey
    * @type number
    */
-  age: number;
+  rowKey: string;
+
+  /**
+   * 是否显示搜索表单
+   *
+   * @title 是否显示搜索表单
+   * @type boolean
+   * @default true
+   */
+  search: boolean;
 }
--- rc-table/src/index.tsx
+++ rc-table/src/index.tsx
@@ -1,15 +1,138 @@
 import './index.scss';
 import React from "react";
 import IProps from "../IProps";
+import {ProColumns, ProTable, TableDropdown} from "@ant-design/pro-table";
+import {Button, message, Space, Tag} from "antd";
+import {PlusOutlined} from "@ant-design/icons";
+import request from 'umi-request';
 
 interface Props extends IProps {
   style: React.CSSProperties;
 }
+
+
+type GithubIssueItem = {
+  url: string;
+  id: number;
+  number: number;
+  title: string;
+  labels: {
+    name: string;
+    color: string;
+  }[];
+  state: string;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at?: string;
+};
+
+
+const columns: ProColumns<GithubIssueItem>[] = [
+  {
+    title: '序号',
+    dataIndex: 'index',
+    width: 64,
+    valueType: 'indexBorder',
+  },
+  {
+    title: '标题',
+    dataIndex: 'title',
+    copyable: true,
+    ellipsis: true,
+    search: false,
+  },
+  {
+    title: (_, type) => (type === 'table' ? '状态' : '列表状态'),
+    dataIndex: 'state',
+    initialValue: 'all',
+    filters: true,
+    onFilter: true,
+    valueType: 'select',
+    valueEnum: {
+      all: { text: '全部', status: 'Default' },
+      open: {
+        text: '未解决',
+        status: 'Error',
+      },
+      closed: {
+        text: '已解决',
+        status: 'Success',
+      },
+    },
+  },
+  {
+    title: '排序方式',
+    key: 'direction',
+    hideInTable: true,
+    hideInDescriptions: true,
+    dataIndex: 'direction',
+    filters: true,
+    onFilter: true,
+    valueType: 'select',
+    valueEnum: {
+      asc: '正序',
+      desc: '倒序',
+    },
+  },
+  {
+    title: '标签',
+    dataIndex: 'labels',
+    width: 120,
+    render: (_, row) => (
+      <Space>
+        {row.labels.map(({ name, color }) => (
+          <Tag color={color} key={name}>
+            {name}
+          </Tag>
+        ))}
+      </Space>
+    ),
+  },
+  {
+    title: 'option',
+    valueType: 'option',
+    dataIndex: 'id',
+    render: (_text, row) => [
+      <a href={row.url} key="show" target="_blank" rel="noopener noreferrer">
+        查看
+      </a>,
+      <TableDropdown
+        key="more"
+        onSelect={(key) => message.info(key)}
+        menus={[
+          { key: 'copy', name: '复制' },
+          { key: 'delete', name: '删除' },
+        ]}
+      />,
+    ],
+  },
+];
+
 export default (_props: Props): React.JSX.Element => {
-  const { style = {}} = _props;
   return (
-    <div style={style}>
-      待开发
-    </div>
+    <ProTable<GithubIssueItem>
+      columns={columns}
+      type={'table'}
+      request={async (params) => {
+        return request<{
+        data: GithubIssueItem[];
+      }>('https://proapi.azurewebsites.net/github/issues', {
+        params,
+      })
+      }}
+      pagination={{
+        pageSize: 5,
+      }}
+      rowKey="id"
+      dateFormatter="string"
+      headerTitle="查询 Table"
+      toolBarRender={() => [
+        <Button key="3" type="primary">
+          <PlusOutlined />
+          新建
+        </Button>,
+      ]}
+    />
   )
 }
