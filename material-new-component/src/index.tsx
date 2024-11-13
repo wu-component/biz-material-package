39f07c030c4e00e24f3940f702881c7884381d15
Marvin <yongfeide123@gmail.com>
2024-11-13 22:47:16 +0800

    build new version 1.0.1

--- material-new-component/src/index.tsx
+++ material-new-component/src/index.tsx
@@ -7,17 +7,24 @@ export default (_props: IProps): React.JSX.Element => {
   return (
     <div className={'material-new-component-container'}>
       <p onClick={() => {
-        onClick?.(1);
+        if (onClick) {
+          onClick?.(1);
+        }
       }}>一级属性点击 {name}</p>
       <p onClick={() => {
-        item?.onClick?.(1);
+        if (item?.onClick) {
+          item?.onClick?.(1);
+        }
+
       }}>对象属性中配置了方法： {item?.name}</p>
       <div className={'content-list'}>
         {
           list.map((t, i) => {
             return (
-              <div className={'item'} onClick={() => {
-                t?.onClick?.(i)
+              <div className={'item'} key={i} onClick={() => {
+                if (t?.onClick) {
+                  t?.onClick?.(i)
+                }
               }}>
                 列表配置事件： {
                   t.name
